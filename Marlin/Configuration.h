0501757d50cf7465c6d87302346e6c0bf62d2e4e
Ryan McDonnell <ryan@ryanmcdonnell.com>
2020-03-10 22:01:13 -0700

    Add PID autotune results

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -485,9 +485,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  //#define DEFAULT_Kp 22.2
+  //#define DEFAULT_Ki 1.08
+  //#define DEFAULT_Kd 114
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -498,6 +498,11 @@
   //#define DEFAULT_Kp 63.0
   //#define DEFAULT_Ki 2.25
   //#define DEFAULT_Kd 440
+
+  // binaryryan - Titan Aero w/ PLA @ 200F
+  #define DEFAULT_Kp 18.82
+  #define DEFAULT_Ki 1.43
+  #define DEFAULT_Kd 61.93
 
 #endif // PIDTEMP
 
