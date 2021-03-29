6247961c269e0633a4cf1282f40b3323a2baa039
Ryan McDonnell <ryan@binaryfactory.com>
2021-03-29 15:44:53 -0700

    Adjust nozzle offset for Mosquito DDX with BL-touch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, 9, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -27.625, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
